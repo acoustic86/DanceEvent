78f0700f116e540c45a2959655d6501905ad0bb9
Alex Kozlov <arcadiabakery@gmail.com>
2013-09-01 17:08:55 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DanceEvent::Application.config.secret_key_base = '4f3e4fac45fd8e66b1913a3c3005c3a57f01d266b9952210b96a2f0f4aab01f0f0a61fe132568ae4feb6fe53b2c8f7b40ccfe1600978787301cde3082b370936'
