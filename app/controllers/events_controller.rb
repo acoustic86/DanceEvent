ccae41dff45a3995cc5a9feba04471a9145dd9d3
Alex Kozlov <arcadiabakery@gmail.com>
2013-09-01 19:38:55 -0700

    Added Devise Views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   # GET /events
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ DanceEvent::Application.routes.draw do
   devise_for :users
   resources :events
   
-  root to: 'events#index'
+  root to: "events#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
